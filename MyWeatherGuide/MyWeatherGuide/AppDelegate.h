6ebdb60e8b22c1ad8b377c4d77bb2a285455fd77
angryboy <535162703@qq.com>
2016-01-30 09:42:31 +0800

    提交

---
+++ MyWeatherGuide/MyWeatherGuide/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyWeatherGuide
+//
+//  Created by lanouhn on 16/1/30.
+//  Copyright © 2016年 lanouhn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyWeatherGuide/MyWeatherGuide/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyWeatherGuide
+//
+//  Created by lanouhn on 16/1/30.
+//  Copyright © 2016年 lanouhn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
